ece61fb688fb5722d84dcde9de82239c545cdae9
Kernael92 <kernaelapuko5@gmail.com>
2018-11-13 10:56:08 +0300

    creating a secure password

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config:
     MOVIE_API_BASE_URL = 'http://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kernaeljoy:benter92@localhost/watchlist'
     
 class ProdConfig(Config):
     '''
